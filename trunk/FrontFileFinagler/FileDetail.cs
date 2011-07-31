cc37eea96e0bf3f43eac25cf5ef1289314ec2d17
edgarverona@gmail.com <edgarverona@gmail.com@67a5a537-c091-2293-bea8-2be075fefa49>
2011-07-31 02:33:17 +0000

    Initial commit!  Bare bones features, changing all the dates in-place based on the list of files.

---
+++ trunk/FrontFileFinagler/FileDetail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FrontFileFinagler
+{
+    public class FileDetail
+    {
+
+        public string OriginalPath { get; private set; }
+        public string FileName { get; private set; }
+
+
+        public DateTime CreationTime 
+        {
+            get
+            {
+                return File.GetCreationTime(this.OriginalPath);
+            }
+            set
+            {
+                File.SetCreationTime(this.OriginalPath, value);
+            }
+        }
+
+
+        public DateTime AccessTime
+        {
+            get
+            {
+                return File.GetLastAccessTime(this.OriginalPath);
+            }
+            set
+            {
+                File.SetLastAccessTime(this.OriginalPath, value);
+            }
+        }
+
+        public DateTime WriteTime
+        {
+            get
+            {
+                return File.GetLastWriteTime(this.OriginalPath);
+            }
+            set
+            {
+                File.SetLastWriteTime(this.OriginalPath, value);
+            }
+        }
+
+        
+        public FileDetail(string filePath)
+        {
+            this.OriginalPath = filePath;
+            this.FileName = filePath.Substring(filePath.LastIndexOf(@"\") + 1);
+        }
+
+
+        
+    }
+}
